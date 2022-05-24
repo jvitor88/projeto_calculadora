5025901604add0b5a5161267a4dbab074e8e1d4c
jvitor88 <jvsilveira11@gmail.com>
2022-05-23 23:26:53 -0300

    Att para evitar quebras

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function inserir(num){
     var numero = document.getElementById('resultado').innerHTML
+    if(numero.length < 9){
     document.getElementById('resultado').innerHTML = numero + num
+    }
+    else if(num == '+' || num == '-' || num == '*' || num == '/' ){
+    document.getElementById('resultado').innerHTML = num
+    }
 }
 function limpar(){
     var res = document.getElementById('resultado')
@@ -8,12 +13,10 @@ function limpar(){
 }
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado)
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado).toFixed(2)
     }
-    else
-    {
+    else{
         document.getElementById('resultado').innerHTML = ""
     }
 }
